736626a967a3a94498d5ce7669b84dccfb76dfa1
na <25114018+Natronium@users.noreply.github.com>
2023-01-16 09:02:02 -0500

    init bepinex5plugin project from template
    using -T netstandard2.0 and -U 2019.2.21

---
+++ Plugin.cs
@@ -0,0 +1,14 @@
+using BepInEx;
+
+namespace LogRngCalls
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
